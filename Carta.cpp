d3dc75f693ceea4611f7d02678456b0d8db2fbdc
Pc Sebastian <sebastianserna1126194@correo.itm.edu.co>
2025-03-26 16:29:39 -0500

    Clase Carta.cpp con el método constructor

---
+++ Carta.cpp
@@ -0,0 +1,15 @@
+#include "carta.h"
+
+Carta::Carta() {
+    this->poder = 0;
+    this->color = 0;
+}
+
+Carta::Carta(int poder, int color) {
+    this->poder = poder;
+    this->color = color;
+}
+
+void Carta::mostrar() {
+    std::cout << WHITE << paleta[color] << poder << RESET << " ";
+}
