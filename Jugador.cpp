5ecdd856f95d7a4518461f5a9600dd6c3f8c0a6e
Diego-Alejo <diegoalejandrovelezvelasquez@gmail.com>
2025-03-26 17:23:04 -0500

    Creación clase Jugador.cpp

---
+++ Jugador.cpp
@@ -0,0 +1,35 @@
+//Clase Jugador.cpp
+
+#include "jugador.h"
+#include <iostream>
+
+Jugador::Jugador(std::string nombre) {
+	this->nombre = nombre;
+	cartasGanadas = 0;
+	}
+
+void Jugador::recibirCarta(Carta carta) {
+	mano.push_back(carta);
+	}
+
+void Jugador::mostrarMano() {
+	std::cout << nombre << " - Cartas en mano: " << mano.size() << std::endl;
+	for (int i = 0; i < mano.size(); i++) {
+		std::cout << i + 1 << ": ";
+		mano[i].mostrar();
+		std::cout << std::endl;
+		}
+	}
+
+Carta Jugador::jugarCarta(int indice) {
+	if (indice < 0 || indice >= mano.size()) {
+		std::cout << "Índice inválido." << std::endl;
+		return Carta(0, 0);
+		}
+
+	Carta carta = mano[indice];
+	mano.erase(mano.begin() + indice);
+	return carta;
+	}
+
+//fin clase Jugador.cpp
