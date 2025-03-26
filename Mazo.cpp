5086134eac12ca7d8d1afaaebd1233431744ba8c
Pc Sebastian <sebastianserna1126194@correo.itm.edu.co>
2025-03-26 16:55:07 -0500

    Clase Mazo.cpp

---
+++ Mazo.cpp
@@ -0,0 +1,52 @@
+//Clase Mazo.cpp
+#include "mazo.h"
+#include <cstdlib>
+#include <ctime>
+
+Mazo::Mazo(){
+	int indice = 0;
+	n = 36;
+
+	for(int i = 1; i < 9; i++) {
+		for(int j = 0; j < 4; j++) {
+			indice = (4*i)+j;
+			Cartas[indice].poder = i+1;
+			Cartas[indice].color = j;
+			}
+		}
+	}
+
+void Mazo::mostrar() {
+	int indice = 0;
+
+	for(int i = 0; i < 9; i++) {
+		for(int j = 0; j < 4; j++) {
+			indice = (4*i)+j;
+			Cartas[indice].mostrar();
+			}
+		std::cout << std::endl;
+		}
+	}
+
+void Mazo::barajar() {
+	srand(time(NULL));
+	for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        
+	// Intercambiar Cartas[i] y Cartas[j]
+        
+	Carta temp = Cartas[i];
+        Cartas[i] = Cartas[j];
+        Cartas[j] = temp;
+	}
+}
+
+Carta Mazo::repartir() {
+	if( n<= 0){
+		std::cout << "No hay más cartas en el mazo." << std::endl;
+        return Carta(0, 0);
+    }
+
+    n--;
+    return Cartas[n];
+}
