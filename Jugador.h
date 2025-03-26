523338cf59f1dabb6ea1e8529485f855aac5a528
Diego-Alejo <diegoalejandrovelezvelasquez@gmail.com>
2025-03-26 17:16:53 -0500

    Modificación clase Jugador.h

--- Jugador.h
+++ Jugador.h
@@ -1,1 +1,24 @@
-//hola mundo
+//Clase jugador.h
+
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include <vector>
+#include <string>
+#include "carta.h"
+
+class Jugador {
+	public:
+	std::vector<Carta> mano;
+	int cartasGanadas;
+	std::string nombre;
+
+	Jugador(std::string nombre);
+	void recibirCarta(Carta carta);
+	void mostrarMano();
+	Carta jugarCarta(int indice);
+
+};
+
+
+#endif //
